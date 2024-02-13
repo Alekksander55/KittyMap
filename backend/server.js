aa64da97d1e1683a92334a62f6a7af656520cfe9
Alekksander55 <alexandertemim@gmail.com>
2024-02-13 11:25:25 +0200

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -12,34 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
+
 app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/users", userRoutes);
 app.use("/api/markers", markerRoutes);
-const corsOptions = {
-  origin: 'https://kittymap-fe.onrender.com',
-  
-};
 
-app.use(cors(corsOptions));
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://kittymap-fe.onrender.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use(notFound);
 app.use(errorHandler);
