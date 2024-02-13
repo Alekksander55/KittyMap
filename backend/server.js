71e4fbff87a69b5889aa399c300e985c16cf9a69
Alekksander55 <alexandertemim@gmail.com>
2024-02-13 10:47:51 +0200

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
-import markerRoutes from './routes/markerRoutes.js'
+import markerRoutes from "./routes/markerRoutes.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import connectToDb from "./config/db.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/users", userRoutes);
-app.use('/api/markers', markerRoutes)
-app.use(cors())
+app.use("/api/markers", markerRoutes);
+const corsOptions = {
+  origin: /\.onrender\.com$/,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsOptions));
 
 app.use(notFound);
 app.use(errorHandler);
